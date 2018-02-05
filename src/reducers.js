8fc9d589f571a4afb839b9589304a5ab10d21235
Eline <eline.ax@gmail.com>
2018-02-05 18:48:38 -0200

    Cleaned reducers.js file

--- src/reducers.js
+++ src/reducers.js
@@ -1,86 +1,23 @@
 import {
-   ADD_CATEGORIES
- } from './actions'
+  ADD_CATEGORIES
+} from './actions'
 
- const initialState = {
-   categories: [],
-   posts: {},
-   comments: {}
- }
+const initialState = {
+  categories: [],
+  posts: {},
+  comments: {}
+}
 
- function categories(state = initialState, action){
-   switch (action.type){
-     case ADD_CATEGORIES:
-       return {
-         ...state,
-         categories: action.categoriesList
-       }
-     default:
-       return state
-   }
- }
+function categories(state = initialState, action){
+  switch (action.type){
+    case ADD_CATEGORIES:
+      return {
+        ...state,
+        categories: action.categoriesList
+      }
+    default:
+      return state
+  }
+}
 
-  // const initialCalendarState = {
-  //   sunday: {
-  //     breakfast: null,
-  //     lunch: null,
-  //     dinner: null,
-  //   },
-  //   monday: {
-  //     breakfast: null,
-  //     lunch: null,
-  //     dinner: null,
-  //   },
-  //   tuesday: {
-  //     breakfast: null,
-  //     lunch: null,
-  //     dinner: null,
-  //   },
-  //   wednesday: {
-  //     breakfast: null,
-  //     lunch: null,
-  //     dinner: null,
-  //   },
-  //   thursday: {
-  //     breakfast: null,
-  //     lunch: null,
-  //     dinner: null,
-  //   },
-  //   friday: {
-  //     breakfast: null,
-  //     lunch: null,
-  //     dinner: null,
-  //   },
-  //   saturday: {
-  //     breakfast: null,
-  //     lunch: null,
-  //     dinner: null,
-  //   },
-  // }
-  //
-  // function calendar (state = initialCalendarState, action) {
-  //   const { day, recipe, meal } = action
-  //
-  //   switch (action.type) {
-  //     case ADD_RECIPE :
-  //       return {
-  //         ...state,
-  //         [day]: {
-  //           ...state[day],
-  //           [meal]: recipe.label,
-  //         }
-  //       }
-  //     case REMOVE_FROM_CALENDAR :
-  //       return {
-  //         ...state,
-  //         [day]: {
-  //           ...state[day],
-  //           [meal]: null,
-  //         }
-  //       }
-  //     default :
-  //       return state
-  //   }
-  // }
-
-  export default categories
+export default categories
