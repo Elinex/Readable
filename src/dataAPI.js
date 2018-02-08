b929a6ef7245a35ed065f54feb85efa42805866c
Eline <eline.ax@gmail.com>
2018-02-08 21:02:45 -0200

    Created postPosts and getPosts request API functions

--- src/dataAPI.js
+++ src/dataAPI.js
@@ -5,10 +5,23 @@ const headers = {
   'Authorization': 'Whatever'
 }
 
+const headersPost = {
+  'Accept': 'application/json',
+  'Authorization': 'Whatever',
+  'Content-Type': 'application/json'
+}
+
 export const getCategories = () =>
   fetch(`${api}/categories`, { headers })
     .then(res => res.json())
 
-export const addPost = () =>
-  fetch(`${api}/posts`, { headers })
-    .then(res => res.json())
+export const postPost = (post) =>
+  fetch(`${api}/posts`, {
+    method: 'POST',
+    headers: headersPost,
+    body: JSON.stringify(post)
+  }).then(res => res.json())
+
+  export const getPosts = () =>
+    fetch(`${api}/posts`, { headers })
+      .then(res => res.json())
