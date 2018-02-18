060ec49089e0a4d311249a5c47d1a4f69f41f75a
Eline <eline.ax@gmail.com>
2018-02-18 18:02:54 -0300

    Created Comment component

---
+++ src/Comment.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Comment extends Component{
+
+  render (){
+
+    return (
+      <div>
+        I am a comment
+      </div>
+    )
+  }
+}
+
+export default Comment
