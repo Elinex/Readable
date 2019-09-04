a120bc8608938b967fdced966db645c7ebd6ec27
Elinex <eline.ax@gmail.com>
2019-09-03 20:32:03 -0700

    remove compose function from index.js

--- src/index.js
+++ src/index.js
@@ -12,10 +12,11 @@ import logger from 'redux-logger'
 
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk, logger),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  applyMiddleware(thunk, logger)
+  // compose(
+    // applyMiddleware(thunk, logger),
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // )
 )
 
 console.log(store.getState())
